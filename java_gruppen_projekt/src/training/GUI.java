0235613dbcf034ddb951c7d3d2e93216be8615ad
andylehoang <42783594+andylehoang@users.noreply.github.com>
2024-01-11 19:42:24 +0000

    weitere Test on Codespace

--- java_gruppen_projekt/src/training/GUI.java
+++ java_gruppen_projekt/src/training/GUI.java
@@ -4,7 +4,8 @@ public class GUI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello World");
+		Test myTest = new Test();
+        System.out.println(myTest.getX());
 	}
 
 }
---
+++ java_gruppen_projekt/src/training/Test.java
@@ -0,0 +1,9 @@
+package training;
+
+public class Test {
+    private String x = "Hello World";
+    
+    public String getX () {
+        return this.x;
+    }
+}
