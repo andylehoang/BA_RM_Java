1e718b40cbe34019c498c6486fa42f355ce2d11f
NielsCBohr <158182812+NielsCBohr@users.noreply.github.com>
2024-02-22 14:21:32 +0000

    Javadoc v1

--- java_gruppen_projekt/src/de/ba_supermarkt/Haushaltartikel.java
+++ java_gruppen_projekt/src/de/ba_supermarkt/Haushaltartikel.java
@@ -5,20 +5,26 @@ package de.ba_supermarkt;
 
 // TODO: Auto-generated Javadoc
 /**
- * The Class Haushaltartikel.
+ * @author Andy LeHoang, Niels Bohr und Julian Schleich.
+ * Dies ist die Klasse Haushaltsartikel. Die Prdoukte mit der Kategorie Haushaltsartikel sollen als Objekte dieser Klasse Haushaltsartikel erstellt werden.
+ * @see Bei der Klasse Haushaltsartikel handelt es sich um eine Tochterklasse der Klasse Waren.  
  */
 public class Haushaltartikel extends Waren{
 	
-	/** The recyling anteil. */
+	/** 
+     * Das Attribut reycclingAnteil wird deklariert.
+     * Dieses Attribut ist der Recyclinganteil.
+     */
 	private int recylingAnteil;
 	
     /**
-     * Instantiates a new haushaltartikel.
-     *
-     * @param name the name
-     * @param EK the ek
-     * @param VK the vk
-     * @param anteile the anteile
+     * Dies ist der Konstruktor der Klasse Haushaltartikel.
+     * Die Attribute name, EK und VK werden von der Klasse Waren vererbt.
+     * Das Attribut recyclingAnteil wird initialisiert. 
+     * @param name der Name des Objektes.
+     * @param EK der Einkaufpreis des Objektes.
+     * @param VK der Verkaufspreis des Objektes.
+     * @param anteile der Recyclinganteil des Objektes.
      */
     public Haushaltartikel(String name, double EK, double VK, int anteile) {
     	super(name,EK,VK);
@@ -26,9 +32,9 @@ public class Haushaltartikel extends Waren{
     }
     
     /**
-     * Gets the recyling anteile.
+     * Diese Methode gibt den Wert von recyclingAnteil zurück.
      *
-     * @return the recyling anteile
+     * @return den Wert von recyclingAnteil. 
      */
     public int getRecylingAnteile() {
     	return this.recylingAnteil;
